9e5bdb07748e0d3280ad16b0afdbb89032375632
Gideon Nolte <gideon@dienoltens.de>
2022-01-11 18:59:35 +0100

    Added Macro Configuration and extended Features

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,27 +1,57 @@
 import { fetch } from '@forge/api';
 
-const random_url = (highest_id) => {
-    return `https://xkcd.com/${Math.floor(Math.random() * highest_id) + 1}`
+const get_random_number = (min, max) => {
+    [min, max] = [Number(min), Number(max)]
+    return Math.round(Math.random() * (max-min)) + min
 }
 
-const load_xml = async (path) => {
-   let resp = await fetch(path);
-   let text = await resp.text();
-   return text
-}
+const load = async (url) => {
+    let resp = await fetch(url);
+    let text = await resp.text();
+    return text
+ }
 
 const get_most_recent_id = async () => {
-    let rss_feed = await load_xml('https://xkcd.com/rss.xml');
+    let rss_feed = await load('https://xkcd.com/rss.xml');
     return Number(rss_feed.match(/https:\/\/xkcd.com\/(\d{4,5})/)[1]); //Capturing Route Id of latest item in feed
 }
+const random_url = async () => {
+    let highest_id = await get_most_recent_id();
+    console.log(`Highest ID: ${highest_id}`)
+    return `https://xkcd.com/${get_random_number(1, highest_id)}`
+}
 
-const parse_random_webpage = async (most_recent) => {
-    let comic_page = await load_xml(random_url(most_recent));
-    let image_url = comic_page.match(/<img src="(\/\/imgs.xkcd.com\/comics\/.*?\.png)"/)[1];
-    return image_url
+const specific_url = (id) => {
+    return `https://xkcd.com/${id}`
+}
+
+const parse_webpage = async (url) => {
+    let id = url.match(/https:\/\/xkcd.com\/(\d{1,5})/)[1]
+    let comic_page = await load(url);
+    let image_url = comic_page.match(/<img src="(\/\/imgs.xkcd.com\/comics\/.*?[\.png|\.jpg|\.jpeg])"/)[1];
+    let alt_text = comic_page.match(/<img src="\/\/imgs.xkcd.com\/comics\/.*?[\.png|\.jpg|\.jpeg]" .*? alt="(.*?)" .*/)[1];
+    return {'src': `https:${image_url}`, 'alt': alt_text, 'url' : url, 'id': id }
 } 
 
-export const get_random_xkcd_url = async () => {
-    let most_recent = await get_most_recent_id()
-    return await parse_random_webpage(most_recent)
+export const get_xkcd_image_url = async (selection) => {
+    let url;
+    if (selection) {
+        if (selection.includes(',')) { //Check if selection contains more than one ID
+            console.log(`Current Selection is: ${selection}`);
+            selection = selection.split(','); //Make selection an array
+            selection = selection[get_random_number(0, selection.length-1)]; //Retrieve random object
+        }
+        if (selection.includes('-')) { //Check if selection is range
+            console.log(`Current Selection is: ${selection}`);
+            let [min,max] = selection.split('-'); //Get range limits
+            console.log(`Current Range is: ${min} - ${max}`);
+            selection = get_random_number(min,max);
+            console.log(`Current Selection is: ${selection}`);
+         }
+        url = specific_url(selection);
+    } else {
+        url = await random_url();
+    }
+    console.log(`Current URL is: ${url}`);
+    return await parse_webpage(url)
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,41 @@
-import ForgeUI, { render, Fragment, Macro, useState, Image } from "@forge/ui";
-import { get_random_xkcd_url } from "./helpers/client";
+import ForgeUI, { render, Fragment, Macro, useState, Image, Text, Strong, Link, MacroConfig, TextField, useConfig, RadioGroup, Radio } from "@forge/ui";
+import { get_xkcd_image_url } from "./helpers/client";
 
 const App = () => {
-  const [src] = useState(async () => await get_random_xkcd_url());
+  const fallbackConfig = {
+      selection: undefined,
+      showTitle: 'no'
+    };
+  const config = useConfig() || fallbackConfig;
+  console.log(`Current Config: ${config.toString()}`);
+  const [data] = useState(async () => await get_xkcd_image_url(config.selection));
+
+  if (config.showTitle == 'yes') {
+      return (
+        <Fragment>
+            <Text><Link href={data.url}>{data.id}: <Strong>{data.alt}</Strong></Link></Text>
+            <Image src={data.src} alt={data.alt}/>
+        </Fragment>
+      );
+  };
   return (
     <Fragment>
-       <Image
-        src={src}
-      />
+        <Image src={data.src} alt={data.alt}/>
     </Fragment>
   );
+};
+
+
+const Configuration = () => {
+    return (
+        <MacroConfig>
+            <RadioGroup label="Show Title" name="showTitle">
+               <Radio value="yes" label="Yes" />
+               <Radio defaultChecked value="no" label="No" />
+            </RadioGroup>
+            <TextField name="selection" label="Comic IDs to select from" description="Enter the IDs of the Comics you would like to see. You can specify multiple IDs and ranges of IDs like this: 1409-1412,1540,2170,560-590" placeholder="1409-1412,1540,2170,560-590"></TextField>
+        </MacroConfig>
+    );
 };
 
 export const run = render(
@@ -17,3 +43,5 @@ export const run = render(
     app={<App />}
   />
 );
+
+export const configure = render(<Configuration/>);
